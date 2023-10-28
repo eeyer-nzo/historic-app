5adff93ed5b2665e8309525781f57fd6c74cff7c
eeyer-nzo <enzooh8888@gmail.com>
2023-10-28 09:27:06 +0800

    Initial Commit

---
+++ historic app/historic_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  historic_appApp.swift
+//  historic app
+//
+//  Created by T Krobot on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct historic_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
