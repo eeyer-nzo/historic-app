164198cc23fffc0cf25dc2d957b306f62271a8fb
Arthan Tjen <arthan.nathaniel.tjen@sji.edu.sg>
2023-11-17 09:49:40 +0800

    The code 2.0 1711949

--- historic app/ContentView.swift
+++ historic app/ContentView.swift
@@ -1,27 +1,52 @@
 import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView()
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        // You can customize the map view here if needed
+        let coordinate = CLLocationCoordinate2D(latitude: 1.2540, longitude: 103.8234)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "Sentosa"
+        uiView.addAnnotation(annotation)
+    }
+}
 
 struct ContentView: View {
     @State private var showSheet = false
 
     var body: some View {
-        VStack {
-            Text("MAP")
-                .font(.largeTitle)
-                .padding()
-                
-            Button("Show Sheet") {
-                // Toggle the showSheet state
-                showSheet.toggle()
-            }
-            .padding()
+        GeometryReader { geometry in
+            VStack {
+                MapView()
+                    .edgesIgnoringSafeArea(.all) // Ignore safe area edges
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .offset(y: -geometry.safeAreaInsets.top) // Adjust the top offset
 
-            // Example of a sheet
-            .sheet(isPresented: $showSheet) {
-                Text("Sentosa!")
-                    .padding()
+                Spacer() // Move the rest of the content to the bottom
+
+                Button("Show Sheet") {
+                    // Toggle the showSheet state
+                    showSheet.toggle()
+                }
+                .padding()
+
+                // Example of a sheet
+                .sheet(isPresented: $showSheet) {
+                    Text("Sentosa!")
+                        .padding()
+                }
             }
         }
-        .padding()
+        .edgesIgnoringSafeArea(.top) // Ignore safe area edges for the VStack
     }
 }
 
