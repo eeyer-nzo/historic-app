6295021fee5a5c37461bc99f87339deee57a1672
Arthan Tjen <arthan.nathaniel.tjen@sji.edu.sg>
2023-11-21 16:09:51 +0800

    not done dont pull the lets at custom sheet are wrong help

--- historic app/ContentView.swift
+++ historic app/ContentView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 import MapKit
 //You should know
+
 //IDK someone fill this in
 struct MapView: UIViewRepresentable {
     @Binding var zoomInOut: Double
@@ -13,7 +14,7 @@ struct MapView: UIViewRepresentable {
         let span = MKCoordinateSpan(latitudeDelta: zoomInOut, longitudeDelta: zoomInOut)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         uiView.setRegion(region, animated: true)
-            
+        
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinate
         annotation.title = "Sentosa"
@@ -41,10 +42,10 @@ struct ContentView: View {
         ZStack {
             
             MapView(zoomInOut: $zoomInOut)
-                .edgesIgnoringSafeArea(.top)
+                .edgesIgnoringSafeArea(.all)
             
             VStack{
-                 
+                
                 VStack(spacing: 0) {
                     HStack{
                         Button{
@@ -70,81 +71,83 @@ struct ContentView: View {
                         }
                     }
                     //DO NOT CHANGE WTV CODE IS FROM HERE{
-                        .gesture(
-                            DragGesture()
-                                .onChanged { value in
-                                    // Update sheet position based on drag gesture
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                // Update sheet position based on drag gesture
+                                withAnimation {
+                                    sheetPosition = max(min(value.translation.height, 0), -UIScreen.main .bounds.height)
+                                }
+                            }
+                        
+                            .onEnded { value in
+                                // Determine whether to show or hide the sheet based on drag distance
+                                let threshold: CGFloat = -100
+                                
+                                //BTW you can edit the code but ONLY the one below if u understand whats going on wtv is above DO NOT TOUCH is the sheet movement code.
+                                
+                                //What happens when the sheet goes up
+                                
+                                withAnimation(.bouncy){ //only Xcode 15.0 has snappy animation, so yall will get an error. How to fix: get rid of "(.snappy)" yes the brackets too. Theres another one down at 3 paragraphs.
+                                    
+                                    chkShtTbPos = true
+                                    if chkShtTbPos == true{
+                                        spacerMnLngth = -300
+                                    }
+                                }
+                                if value.translation.height < threshold {
                                     withAnimation {
-                                        sheetPosition = max(min(value.translation.height, 0), -UIScreen.main .bounds.height)
+                                        // This controls how high the sheet goes up btw higher value decreases height 不要问
+                                        sheetPosition =  -UIScreen.main.bounds.height
+                                        + 560
+                                        showSheet = false
+                                        
                                     }
+                                } else { // What happens when sheet goes down
+                                    
+                                    withAnimation(.bouncy){
+                                        //only Xcode 15.0 has snappy animation, so yall will get an error. How to fix: get rid of "(.snappy)" yes the brackets too
+                                        
+                                        chkShtTbPos = false
+                                        if chkShtTbPos == false{
+                                            spacerMnLngth = 300
+                                        }
+                                    }
+                                    
+                                    withAnimation {
+                                        sheetPosition = 0
+                                        showSheet = true
+                                    }
+                                    
                                 }
-                            
-                                .onEnded { value in
-                                    // Determine whether to show or hide the sheet based on drag distance
-                                    let threshold: CGFloat = -100
-                                    
-                                    //BTW you can edit the code but ONLY the one below if u understand whats going on wtv is above DO NOT TOUCH is the sheet movement code.
-                                    
-                                    //What happens when the sheet goes up
-                                    
-                                    withAnimation(.bouncy){ //only Xcode 15.0 has snappy animation, so yall will get an error. How to fix: get rid of "(.snappy)" yes the brackets too. Theres another one down at 3 paragraphs.
-                                        
-                                        chkShtTbPos = true
-                                        if chkShtTbPos == true{
-                                            spacerMnLngth = -300
-                                        }
-                                    }
-                                    if value.translation.height < threshold {
-                                        withAnimation {
-                                            // This controls how high the sheet goes up
-                                            sheetPosition =  -UIScreen.main.bounds.height
-                                            + 560
-                                            showSheet = false
-                                            
-                                        }
-                                    } else { // What happens when sheet goes down
-                                        
-                                        withAnimation(.bouncy){
-                                            //only Xcode 15.0 has snappy animation, so yall will get an error. How to fix: get rid of "(.snappy)" yes the brackets too
-                                            
-                                            chkShtTbPos = false
-                                            if chkShtTbPos == false{
-                                                spacerMnLngth = 300
-                                            }
-                                        }
-                                        
-                                        withAnimation {
-                                            sheetPosition = 0
-                                            showSheet = true
-                                        }
-                                        
-                                    }
-                                }
-                            
-                        )// }TO HERE its basically a .gesture code with a super long parameter details inside
+                            }
+                        
+                    )// }TO HERE its basically a .gesture code with a super long parameter details inside
                     
                     //Creates the tab bar
                     TabView{
-                            // This is shown when the home "Button" is clicked
-                            CustomSheetView()// This is the sheets view like buttons
+                        // This is shown when the home "Button" is clicked
+                        CustomSheetView()// This is the sheets view like buttons
+                        
                         //Creates the home button design
-                        .tabItem {
-                            Label("Home", systemImage: "house")
-                        }
+                            .tabItem {
+                                Label("Home", systemImage: "house")
+                            }
                         FavouritesView()
-                        .tabItem {
-                            Label("Favourites", systemImage: "star.fill")
-                            
-                        }
+                            .tabItem {
+                                Label("Favourites", systemImage: "star.fill")
+                                
+                            }
                         
                     }
                     .gesture(
-                                        DragGesture().onChanged { _ in
-                                            // Do nothing on drag to prevent tab movement
-                                        }
-                                    )
+                        DragGesture().onChanged { _ in
+                            // Do nothing on drag to prevent tab movement
+                        }
+                    )
                     // The spacer 🎵 push and pull like a magnet do 🎵 the Tab Bar
                     Spacer(minLength: spacerMnLngth)
+                    
                 }
             }
             //Vstack ends here
@@ -159,56 +162,60 @@ struct ContentView: View {
 //Edits everthing displayed on the sheet{
 struct CustomSheetView: View {
     let names = ["Fort Canning Park", "CHIJMES", "Fort Siloso", "The Battle Box", "Old Changi Hospital", "National Museum", "Lau Pa Sat", "I wonder"]
+    let address = ["Fort Canning Park", "CHIJMES", "Fort Siloso", "The Battle Box", "Old Changi Hospital", "National Museum", "Lau Pa Sat", "I wonder"]
+    let imageNum = ["Fort Canning Park", "CHIJMES", "Fort Siloso", "The Battle Box", "Old Changi Hospital", "National Museum", "Lau Pa Sat", "I wonder"]
+    let historicalRelevance = ["Fort Canning Park", "CHIJMES", "Fort Siloso", "The Battle Box", "Old Changi Hospital", "National Museum", "Lau Pa Sat", "I wonder"]
+    let nMRT = ["Fort Canning Park", "CHIJMES", "Fort Siloso", "The Battle Box", "Old Changi Hospital", "National Museum", "Lau Pa Sat", "I wonder"]
+    let nSGBs = ["Fort Canning Park", "CHIJMES", "Fort Siloso", "The Battle Box", "Old Changi Hospital", "National Museum", "Lau Pa Sat", "I wonder"]
     @State private var searchText = ""
     var body: some View {
         
         NavigationStack {
             List {
                 ForEach(searchResults, id: \.self) { name in
-                     
-                    NavigationLink {
-                        ScrollView{
-                            Image("monkey")
-                            HStack {
-                                Text(name)
-                                    .font(.system(size: 24))
-                                    .bold()
-                                Text("200km")
-                                    .font(.system(size: 12))
-                                Button {
-                                    //placeholder text, its supposed to put the location into favourites
-                                    print("favourite!")
-                                } label: {
-                                    Label(" ", systemImage: "heart.circle")
-                                    
-                                }
+                       
+                    VStack {
+                        NavigationLink {
+                            ScrollView{
+                                Image("monkey")
                                 
+                                HStack {
+                                     Text(name)
+                                        .font(.system(size: 24))
+                                        .bold()
+                                    Text("200km")
+                                        .font(.system(size: 12))
+                                    Button {
+                                        //placeholder text, its supposed to put the location into favourites
+                                    } label: {
+                                        Label(" ", systemImage: "heart.circle")
+                                    }
+                                }
+                                HStack {
+                                    Text(address[0])
+                                        .font(.system(size: 17))
+                                        .bold()
+                                    Image(systemName: "location.circle.fill")
+                                }
+                                Text(historicalRelevance[])
+                                    .padding(80)
+                                Text("Nearest MRT Station: one-north (CC23)")
+                                Text("Nearest Bus Services: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
+                                Spacer()
                             }
-                            HStack {
-                                Text("2 Fusionopolis Way, Singapore 138634")
-                                    .font(.system(size: 17))
-                                    .bold()
-                                Image(systemName: "location.circle.fill")
-                            }
-                            Text("blablablablabla something about history idk extend this thing alot cookie monster chicken nugget french fry onion ring upsize coca cola zero sugar two plus two equals to four")
-                            Spacer()
-                            Text("Nearest MRT Station: one-north (CC23)")
-                            Text("Nearest Bus Services: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
-                            Spacer()
                         }
-                    }
-                    
-                //FORBIDDEN TO TOUCH PLS DON'T
-                label: {
-                        Text(name)
                         
+                        //FORBIDDEN TO TOUCH PLS DON'T
+                    label: {
+                        Text(name)
+                        }
                     }
                 }
             }
             .navigationTitle("Locations")
             
             .searchable(text: $searchText)
-            
+              
             
             var searchResults: [String] {
                 if searchText.isEmpty {
@@ -218,7 +225,10 @@ struct CustomSheetView: View {
                 }
             }
         }
+        
+        
     }
+    
 }
 //Its that little grey thing on top of the sheet
 struct HandleBar: View {
