e360807151469c96dbcd108a939fc462360f5b31
Arthan Tjen <arthan.nathaniel.tjen@sji.edu.sg>
2023-11-25 14:09:50 +0800

    Thank you JiaChen 2511231409

---
+++ historic app/LocationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsListView.swift
+//  historic app
+//
+//  Created by Arthan Tjen on 25/11/23.
+//
+
+import SwiftUI
+
+struct LocationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsListView()
+}
---
+++ historic app/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  historic app
+//
+//  Created by Arthan Tjen on 25/11/23.
+//
+
+import Foundation
